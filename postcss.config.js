238a06e98743d2a77011eabaf573f4551340f4db
laowu <wuly93@163.com>
2025-02-21 14:13:56 +0800

    feat: 关闭 px2vw插件

--- postcss.config.js
+++ postcss.config.js
@@ -6,11 +6,12 @@ export default {
       stage: 3, // 支持 CSS 草案阶段
     },
     autoprefixer: {}, // 自动前缀
-    'postcss-px-to-viewport': {
-      viewportWidth: 375, // 设计稿宽度
-      unitPrecision: 5,
-      propList: ['*', '!letter-spacing'], // 需要转换的属性列表
-      selectorBlackList: ['.ignore'], // 忽略的类名
-    },
+    // 'postcss-px-to-viewport': { // 移动端项目的自适应配置
+    //   viewportWidth: 375, // 设计稿宽度
+    //   unitPrecision: 5,
+    //   propList: ['*', '!letter-spacing'], // 需要转换的属性列表
+    //   selectorBlackList: ['.ignore'], // 忽略的类名
+    //   // exclude: [/src\/pc/], // 排除多个目录
+    // },
   },
 };
