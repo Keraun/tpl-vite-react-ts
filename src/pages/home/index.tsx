7095cc637e0db90ccdd866d372e78fc1326c1232
laowu <wuly93@163.com>
2025-03-07 17:03:27 +0800

    feat: 测试

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,5 @@
 import { useContext, useEffect } from 'react';
 import { GLOBAL_CONTEXT } from '@/context';
-import { useRequest } from 'ahooks';
-import request from '@/lib/request';
 import DemoComponent from '@/components/demo';
 import useHomeHook from './useHomeHook';
 import './index.less';
@@ -11,17 +9,8 @@ export default function Home() {
   const globalContext = useContext(GLOBAL_CONTEXT);
   const { testData } = useHomeHook({ initTestData: 1 });
 
-  const { data, error, loading } = useRequest(async () => {
-    const response = await request('/api/user', {
-      params: { test: 1 },
-      data: { test: 2 },
-    });
-    return response;
-  });
-
   useEffect(() => {}, []);
 
-  console.log('api mock data', data, error, loading);
   console.log('globalContext', globalContext);
   console.log('useHomeHook testData', testData.current);
 
